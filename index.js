7dae1bb010aa6653589886e8f3d3cf40dd86c91a
Tony Mai <tonymaibox@yahoo.com>
2016-09-22 14:47:46 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 const app = "I don't do much.";
+
+const Button = React.createClass({
+  render() {
+    return React.createElement('button', {}, 'Click me!');
+  }
+});
+
+const ShoppingList = React.createClass({
+  render() {
+    return React.createElement('ul', {}, [
+      React.createElement('li', {}, 'Bananas'),
+      React.createElement('li', {}, 'Vanilla ice cream'),
+      React.createElement('li', {}, 'Chocolate'),
+    ]);
+  }
+});
